21290c9864e155c67b5295ff7e0e04a43038a6ae
surya <suryapradhan559@gmail.com>
2024-09-30 02:32:12 -0700

    jsadv

---
+++ 01-variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+
+let accoountEmail = "sur@dwj "
+
+var accountPassword = "11234"
+
+accountCity = "bbsr"
+
+let accountState; //the result should be undefined
+
+// accountId = 2 // not used due to the const keyword
+
+// mostly used keyword is const and let
+
+// var not used due to scope-->{} 
+
+accoountEmail = "aanmds2xmw"
+
+accountPassword = "676283"
+
+accountCity = "bang"
+
+console.table([accountId,accoountEmail,accountPassword,accountCity,accountState]);
