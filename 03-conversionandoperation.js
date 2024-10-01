7bc128bc3f1dbc764c07ddeb7b92ce186299cfd5
surya <suryapradhan559@gmail.com>
2024-10-01 08:10:42 -0700

    conversioninjs

---
+++ 03-conversionandoperation.js
@@ -0,0 +1,77 @@
+// let score = "surya"
+
+// console.log(typeof score);
+
+// let valueinNumber = String(score);
+
+// console.log(typeof valueinNumber);
+
+// console.log(valueinNumber)
+// "33"=>33
+
+// let isLoggedin = 1
+
+// let loggedOrnot = Boolean (isLoggedin)
+
+// console.log(loggedOrnot)
+
+// 1=>true
+
+// "" =>false
+
+
+// let aNumber = 33
+
+// let verNumb = String(aNumber)
+
+// console.log(typeof verNumb)
+
+// console.log(verNumb)
+
+// let value = 3;
+
+// let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2**2)
+
+// let str1 = "hello"
+
+// let str2 = "surya"
+
+// let str3 = str1+str2;
+
+// console.log(str3)
+
+
+// console.log(1+2+"2") //output is 32
+
+// console.log(1+"2")   //output is 12
+
+// console.log("1"+2)   //output is 12
+
+
+
+// let num1,num2,num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+
+// ++gameCounter;
+
+// console.log(gameCounter)
+
+
+
+
+
+
+
+
+
+
+
+
+
