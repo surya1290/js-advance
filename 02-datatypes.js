4152fb8a261ef30b0368b7bd71be63ce2bea1772
surya <suryapradhan559@gmail.com>
2024-10-01 07:26:37 -0700

    datatypes

---
+++ 02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat code as a new version of js
+
+// alert(3+3) // we are using node js not browser
+
+console.log(3+3);
+
+console.log();
+
+let name = "surya"
+let age  = 81
+let isLoggedin  = true
+// number =>2 to the power of 53
+// bigint
+// string=""
+// boolean
+// null
+// undefined 
+// symbol!@#$
+
+
+// object
+console.log(typeof "hdd")
